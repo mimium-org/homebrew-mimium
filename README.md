cda7b8938a527746410873b43a9f9773feda8e22
Tomoya Matsuura(Parallels Ubuntu) <me@matsuuratomoya.com>
2020-08-22 16:22:41 +0900

    bottle for linux build

--- README.md
+++ README.md
@@ -4,3 +4,15 @@
 brew tap mimium-org/mimium
 brew install mimium
 ```
+
+## On Linux
+
+Default gcc for Linuxbrew, which is version 5.5 may cause conflict with gcc@9 when link with standard library. If you failed to install, try
+
+```bash
+brew unlink gcc@5.5
+brew link gcc@9
+brew install mimium
+brew link gcc@5.5 --overwrite
+```
+
--- mimium.rb
+++ mimium.rb
@@ -8,7 +8,9 @@ class Mimium < Formula
 
   bottle do
     root_url "https://github.com/mimium-org/homebrew-mimium/releases/download/v0.1.0"
-    cellar :any
+    cellar :any_skip_relocation
+    rebuild 1
+    sha256 "2bee9b4aa88427e3a5294c0ba7b87308645ab4fa7377cb649c4fbcc6b9a68c3a" => :x86_64_linux
     sha256 "b6352041fe5dfe3864402f416866d1b42eab7c6284d579ed878cf5ca33499d13" => :catalina
   end
 
@@ -28,9 +30,10 @@ class Mimium < Formula
     if OS.mac?
       system "cmake", "-DBUILD_TEST=ON", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}", ".."
     elsif OS.linux?
+      opoo "If you fail to install, try 'brew unlink gcc@5.5 && brew link gcc@9' before install.\
+      After an installation, make sure to do  'brew link gcc@5.5 --overwrite'"
       system "cmake", "-DBUILD_TEST=ON", "-DCMAKE_BUILD_TYPE=Release",
       "-DCMAKE_C_COMPILER=gcc-9", "-DCMAKE_CXX_COMPILER=g++-9",
-      '-DCMAKE_LINKER_FLAGS="-L/home/linuxbrew/opt/llvm/lib -Wl,-rpath,/home/linuxbrew/.linuxbrew/opt/llvm/lib"',
       "-DCMAKE_INSTALL_PREFIX=#{prefix}", ".."
     end
 
