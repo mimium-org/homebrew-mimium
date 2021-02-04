a0647248c0541a898331d1cb5165578358dc4fce
Tomoya Matsuura(MacBookPro) <me@matsuuratomoya.com>
2021-02-04 21:05:58 +0900

    fix root url

--- mimium.rb
+++ mimium.rb
@@ -8,7 +8,7 @@ class Mimium < Formula
   head "https://github.com/mimium-org/mimium.git", branch: "dev"
 
   bottle do
-    root_url ""
+    root_url "https://dl.bintray.com/tomoyanonymous/bottles-mimium"
   end
 
   depends_on "alsa-lib" unless OS.mac?
