96bf0223e0274ccba34f3017825c35b84f72e590
Tomoya Matsuura(MacBookPro) <me@matsuuratomoya.com>
2021-02-26 07:18:56 +0900

    fixed dependencies

--- mimium.rb
+++ mimium.rb
@@ -21,11 +21,14 @@ class Mimium < Formula
   depends_on "gcc@9" => :build unless OS.mac?
   depends_on "libsndfile" => :build
   depends_on "llvm" => :build
+  depends_on "ncurses" => :build unless OS.mac?
   depends_on "pkg-config" => :build
+  depends_on "zlib" => :build unless OS.mac?
 
   fails_with gcc: "5"
   fails_with gcc: "6"
   fails_with gcc: "7"
+  patch :DATA # patch for linking libsndfile statically in 0.3.1
 
   def install
     mkdir "build"
@@ -51,3 +54,82 @@ class Mimium < Formula
     system "true"
   end
 end
+
+__END__
+
+diff --git a/cmake/FindSndFile.cmake b/cmake/FindSndFile.cmake
+index d19eb4b..86291c8 100644
+--- a/cmake/FindSndFile.cmake
++++ b/cmake/FindSndFile.cmake
+@@ -16,15 +16,14 @@
+ #
+ 
+ if (SNDFILE_LIBRARIES AND SNDFILE_INCLUDE_DIRS)
+-  # in cache already
+-  set(SNDFILE_FOUND TRUE)
++# in cache already
++set(SNDFILE_FOUND TRUE)
+ else (SNDFILE_LIBRARIES AND SNDFILE_INCLUDE_DIRS)
+ if(APPLE)
+ set(HOMEBREW_PATH /usr/local)
+ elseif(UNIX)
+ set(HOMEBREW_PATH /home/linuxbrew/.linuxbrew )
+ else()#windows
+-SET(CMAKE_FIND_LIBRARY_SUFFIXES ".lib" ".dll" ".a.dll" ".dll.a" ".a")
+ set(CMAKE_FIND_LIBRARY_PREFIXES "lib")  
+ endif()
+ find_path(SNDFILE_INCLUDE_DIR
+@@ -42,7 +41,7 @@ find_path(SNDFILE_INCLUDE_DIR
+   
+   find_library(SNDFILE_LIBRARY
+     NAMES
+-      sndfile 
++      libsndfile.a sndfile 
+     PATHS
+       ${HOMEBREW_PATH}/lib
+       /usr/lib
+@@ -55,7 +54,7 @@ find_path(SNDFILE_INCLUDE_DIR
+ 
+   find_library(OGG_LIBRARY
+   NAMES
+-  ogg
++  libogg.a ogg
+   PATHS
+   ${HOMEBREW_PATH}/lib
+   /usr/lib
+@@ -67,7 +66,7 @@ find_path(SNDFILE_INCLUDE_DIR
+   )
+   find_library(VORBIS_LIBRARY
+   NAMES
+-  vorbis 
++  libvorbis.a vorbis 
+   PATHS
+   ${HOMEBREW_PATH}/lib
+   /usr/lib
+@@ -79,7 +78,7 @@ find_path(SNDFILE_INCLUDE_DIR
+   )
+   find_library(VORBISENC_LIBRARY
+   NAMES
+-  vorbisenc
++  libvorbisenc.a vorbisenc
+   PATHS
+   ${HOMEBREW_PATH}/lib
+   /usr/lib
+@@ -91,7 +90,7 @@ find_path(SNDFILE_INCLUDE_DIR
+   )
+   find_library(FLAC_LIBRARY
+   NAMES
+-  flac FLAC
++  libFLAC.a flac FLAC
+   PATHS
+   ${HOMEBREW_PATH}/lib
+   /usr/lib
+@@ -103,7 +102,7 @@ find_path(SNDFILE_INCLUDE_DIR
+   )
+   find_library(OPUS_LIBRARY  REQUIRED
+   NAMES
+-  opus libopus.a
++  libopus.a opus
+   PATHS
+   ${HOMEBREW_PATH}/lib
+   /usr/lib
